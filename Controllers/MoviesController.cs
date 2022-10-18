53ee9bd2b56eb1c8b69d6c8d6d3a16e7b8483264
Ishu Kumar <ishukumar724@gmail.com>
2022-10-18 23:17:38 +0530

    Added put/delete actions

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -44,5 +44,54 @@ namespace AspDotNetCoreApi6.Controllers
 
             return CreatedAtAction(nameof(GetMovieById), new { id = movie.Id }, movie);
         }
+
+        [HttpPut("UpdateMovie/{id}")]
+        public async Task<ActionResult> UpdateMovie(int id, Movie movie)
+        {
+            if(id != movie.Id)
+            {
+                return BadRequest();
+            }
+
+            _movieContext.Entry<Movie>(movie).State = EntityState.Modified;
+
+            try
+            {
+                await _movieContext.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _movieContext.Movies.Any(x => x.Id == id);
+        }
+
+        [HttpDelete("DeleteMovie/{id}")]
+        public async Task<ActionResult> DeleteMovie(int id)
+        {
+            var movie = await _movieContext.Movies.FirstOrDefaultAsync(x => x.Id == id);
+            if(movie == null)
+            {
+                return NotFound();
+            }
+
+            _movieContext.Movies.Remove(movie);
+            await _movieContext.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
